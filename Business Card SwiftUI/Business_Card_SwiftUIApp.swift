4ac9b17318e351b89aded50f6f7846d5bcdda001
parth-nikam <parthsnikam12co@gmail.com>
2023-03-16 17:15:53 +0530

    Initial Commit

---
+++ Business Card SwiftUI/Business_Card_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_Card_SwiftUIApp.swift
+//  Business Card SwiftUI
+//
+//  Created by Parth Nikam on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Business_Card_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
